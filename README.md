f9883a396cc3bada2c52c4458f159d6a88b95970
sinbumu <sinbumu@gmail.com>
2021-05-21 01:13:23 +0900

    sinbumu
    코드 푸시

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # upbit-btc-pizzaday-2021
 pizza gazuaa
+
+
+how to run
+
+0.업비트 마이페이지 > open api 관리에 가서 open api 키 새로 만듬.
+출금조회, 출금하기 체크하고 만들어야 하고 출금하기 때매 특정 ip에서만 실행에
+내가 이걸 돌릴 컴퓨터의 현재 ipv4 주소 채워야 함.
+
+https://upbit.com/mypage/open_api_management
+
+1. set your information ".env" (UPBIT_OPEN_API_ACCESS_KEY, UPBIT_OPEN_API_SECRET_KEY)
+
+2. npm i
+
+3. node index.js
+
+4. 별 이상 없다면 이벤트에 응모 대기를 시작 이라고 뜸...
+그리고 크론잡으로 12:00 에 출금 요청을 쏜다. 
+즉 12시 땡 하기 전에(최소 11:59분 59초엔) 켜놓을것 이후에 키면 출금요청 안함.
+
+**시스템 시간 기준이므로 컴퓨터 시간이 한국시간에 맞춰있어야 함. 아닐경운 코드를 고치건 시각을 바꾸건 알아서 맞추든가 그냥 이 코드 쓰지 말자.
---
+++ index.js
@@ -0,0 +1,84 @@
+const request = require('request')
+const uuidv4 = require("uuid/v4")
+const crypto = require('crypto')
+const sign = require('jsonwebtoken').sign
+const queryEncode = require("querystring").encode
+
+require('dotenv').config();
+
+var cron = require('node-cron');
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY
+const server_url = process.env.UPBIT_OPEN_API_SERVER_URL
+
+const body = {
+    currency: 'BTC'
+}
+
+const query = queryEncode(body)
+
+const hash = crypto.createHash('sha512')
+const queryHash = hash.update(query, 'utf-8').digest('hex')
+
+const payload = {
+    access_key: access_key,
+    nonce: uuidv4(),
+    query_hash: queryHash,
+    query_hash_alg: 'SHA512',
+}
+
+const token = sign(payload, secret_key)
+
+const options = {
+    method: "GET",
+    url: server_url + "/v1/withdraws/chance?" + query,
+    headers: {Authorization: `Bearer ${token}`},
+    json: body
+}
+
+request(options, (error, response, body) => {
+    if (error) throw new Error(error)
+    // console.log(body)
+    const btcBalance = body.account.balance;
+    console.log("your btc balance = ", btcBalance)
+    if(btcBalance >= 0.0001){
+        console.log("이벤트에 응모 대기를 시작.")
+        //// cron job start
+        cron.schedule('0 12 * * *', () => {
+            const body = {
+                currency: 'BTC',
+                amount: '0.0001',
+                address: 'btc-pizzaday-2021',//event address!!!!!!!
+            }
+            
+            const query = queryEncode(body)
+            
+            const hash = crypto.createHash('sha512')
+            const queryHash = hash.update(query, 'utf-8').digest('hex')
+            
+            const payload = {
+                access_key: access_key,
+                nonce: uuidv4(),
+                query_hash: queryHash,
+                query_hash_alg: 'SHA512',
+            }
+            
+            const token = sign(payload, secret_key)
+            
+            const options = {
+                method: "POST",
+                url: server_url + "/v1/withdraws/coin",
+                headers: {Authorization: `Bearer ${token}`},
+                json: body
+            }
+            
+            request(options, (error, response, body) => {
+                if (error) throw new Error(error)
+                console.log(body)
+            })
+        });
+    }else{
+        console.log("이벤트에 응모할 btc가 부족합니다.")
+    }
+})
